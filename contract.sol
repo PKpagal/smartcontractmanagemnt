191a8076803f7d5cdc32bb8439131e3903bd3d83
PKpagal <128849762+PKpagal@users.noreply.github.com>
2024-09-27 00:18:10 +0530

    Update contract.sol

--- contract.sol
+++ contract.sol
@@ -1,110 +1,36 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
 
-contract Assessment {
-    address payable public contractOwner;
-    uint256 public contractBalance;
+contract form {
+    address owner;
 
-    event FundsDeposited(uint256 amount);
-    event FundsWithdrawn(uint256 amount);
+    struct details{
+        string name ;
+        uint age ;
+        string post ;
+    }
+    
+    mapping (address=> details) person_details;
 
-    struct Participant {
-        address participantAddress;
-        uint256 totalWinnings;
+    constructor() {
+        owner = msg.sender;
     }
 
-    constructor(uint initialBalance) payable {
-        contractOwner = payable(msg.sender);
-        contractBalance = initialBalance;
+    modifier onlyowner {
+        require(owner == msg.sender, "only can call this function");
+        _;
     }
 
-    function getContractBalance() public view returns(uint256) {
-        return contractBalance;
+    function setDetails(string memory _name,uint _age,string memory _post) public {
+        person_details[msg.sender].name = _name;
+        person_details[msg.sender].age = _age;
+        person_details[msg.sender].post = _post;
+    }
+    function getDetails() public view returns(string memory,uint,string memory){
+        return (person_details[msg.sender].name,person_details[msg.sender].age,person_details[msg.sender].post);
     }
 
-    function depositFunds(uint256 _amount) public payable {
-        uint256 previousBalance = contractBalance;
-
-        // Ensure that only the owner can deposit funds
-        require(msg.sender == contractOwner, "You are not the owner of this account");
-
-        // Perform the deposit transaction
-        contractBalance += _amount;
-
-        // Ensure the transaction was successful
-        assert(contractBalance == previousBalance + _amount);
-
-        // Emit an event after deposit
-        emit FundsDeposited(_amount);
-    }
-
-    // Custom error for insufficient balance
-    error InsufficientFunds(uint256 balance, uint256 withdrawalAmount);
-
-    function withdrawFunds(uint256 _withdrawAmount) public {
-        require(msg.sender == contractOwner, "You are not the owner of this account");
-
-        uint256 previousBalance = contractBalance;
-        if (contractBalance < _withdrawAmount) {
-            revert InsufficientFunds({
-                balance: contractBalance,
-                withdrawalAmount: _withdrawAmount
-            });
-        }
-
-        // Perform the withdrawal transaction
-        contractBalance -= _withdrawAmount;
-
-        // Ensure the balance is correct after withdrawal
-        assert(contractBalance == (previousBalance - _withdrawAmount));
-
-        // Emit an event after withdrawal
-        emit FundsWithdrawn(_withdrawAmount);
-    }
-
-    // Leaderboard
-    mapping (address => uint256) public winningsRecord;
-    Participant[] public leaderboard;
-
-    function addToLeaderboard(address _address, uint256 amountWon) public {
-        winningsRecord[_address] += amountWon;
-
-        bool alreadyExists = false;
-        for (uint256 i = 0; i < leaderboard.length; i++) {
-            if (leaderboard[i].participantAddress == _address) {
-                leaderboard[i].totalWinnings = winningsRecord[_address];
-                alreadyExists = true;
-                break;
-            }
-        }
-
-        if (!alreadyExists) {
-            leaderboard.push(Participant(_address, winningsRecord[_address]));
-        }
-    }
-
-    function getLeaderboard() public view returns (Participant[] memory) {
-        return leaderboard;
-    }
-
-    // History
-    struct UserTransaction {
-        address user;
-        string outcome;
-        uint256 timestamp;
-    }
-
-    mapping(address => UserTransaction[]) public transactionHistory;
-
-    function addToTransactionHistory(address _user, string memory _outcome) public {
-        transactionHistory[_user].push(UserTransaction({
-            user: _user,
-            outcome: _outcome,
-            timestamp: block.timestamp
-        }));
-    }
-
-    function getTransactionHistory(address _user) public view returns (UserTransaction[] memory) {
-        return transactionHistory[_user];
+    function personDetails(address _address) public view onlyowner returns(string memory,uint,string memory){
+        return (person_details[_address].name,person_details[_address].age,person_details[_address].post);
     }
 }
